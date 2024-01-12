05a789252b36f88cba92a17bcc0cf34d0602ab81
anan99 <54627999+maulachusnan45@users.noreply.github.com>
2024-01-12 14:36:39 +0700

    first commit

---
+++ lib/features/app/splash_screen/splash_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+
+class SplashScreen extends StatefulWidget {
+  final Widget? child;
+  const SplashScreen({super.key, this.child});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+@override
+  void initState() {
+    Future.delayed(
+      Duration(seconds: 3),(){
+        Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => widget.child!), (route) => false);
+    }
+    );
+    super.initState();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          "ZAJAT",
+            style: TextStyle(
+              fontSize: 64,
+              fontWeight: FontWeight.w400,
+              color:  Color(0xffffda2d),
+            ),
+        ),
+      ),
+    );
+  }
+}
